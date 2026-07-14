f518c939a91dfa80c91776b3f1af306c4e9c2915
Soroush Zendedel <soroush_zendedel@Live.com>
2026-07-14 06:45:59 +0330

    DOMContentLoaded and document$

--- docs/javascripts/custom.js
+++ docs/javascripts/custom.js
@@ -1,4 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
+/* استفاده از document$ به جای DOMContentLoaded برای هماهنگی با navigation.instant */
+document$.subscribe(function() {
     /* پیدا کردن دکمه‌های باز و بسته‌شونده‌ای که فقط در سطح اول منوی اصلی هستند */
     var firstLevelToggles = document.querySelectorAll('.md-sidebar--primary .md-nav--primary > .md-nav__list > .md-nav__item--nested > .md-nav__toggle');
     
